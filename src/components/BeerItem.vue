832439a55a3ccc54f5b13790edc6842f20aec8a0
Rookiebutgood <demon-01011930@mail.ru>
2020-10-17 12:37:30 +0600

    add api call, add showmore button, add sass loader

---
+++ src/components/BeerItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="beer-item">
+    <span></span>
+    <img :src="beerInfo.image_url" class="beer-item__img" />
+    <div>
+      <h2>{{beerInfo.name}}</h2>
+      <p>{{beerInfo.description}}</p>
+      <blockquote>{{beerInfo.brewers_tips}}</blockquote>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BeerItem',
+  props: ['beerInfo']
+}
+</script>
+
+<style lang="scss">
+  .beer-item {
+    display: flex;
+    &__img {
+      width: 10%;
+      height: auto;
+    }
+  }
+</style>
--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -1,10 +1,37 @@
 <template>
+<div>
   <p>BeerList</p>
+  <beer-item v-for="(beer, index) in beerList" :key="index" :beerInfo="beer" />
+  <button @click="showMore">Showmore</button>
+</div>
 </template>
 
 <script>
+import BeerItem from '../components/BeerItem.vue';
+import axios from 'axios';
+
 export default {
-  name: 'BeerList'
+  name: 'BeerList',
+  components: { BeerItem },
+  data: function() {
+    return {
+      currentPage: 1,
+      beerList: []
+    }
+  },
+  created: function() {
+    axios.get(`https://api.punkapi.com/v2/beers?page=${this.currentPage}&per_page=5`)
+        .then(response => this.beerList = this.beerList.concat(response.data))
+        .catch(error => console.log(error))
+  },
+  methods: {
+    showMore() {
+      this.currentPage++;
+      axios.get(`https://api.punkapi.com/v2/beers?page=${this.currentPage}&per_page=5`)
+        .then(response => this.beerList = this.beerList.concat(response.data))
+        .catch(error => console.log(error))
+    }
+  }
 }
 </script>
 
